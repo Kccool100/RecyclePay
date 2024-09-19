36fdda1df0db67ecf9b24175c47e1f22e5dee8b8
Kelechi Anyim <kccool2011@yahoo.com>
2024-09-19 20:37:26 +0100

    forgotpassword done

--- src/pages/ForgetPassword/ForgetPassword.jsx
+++ src/pages/ForgetPassword/ForgetPassword.jsx
@@ -1,8 +1,13 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import FP from "../../assets/forgot.png"
 import "./ForgetPassword.css"
-import { Link } from 'react-router-dom'
+import {  useNavigate } from 'react-router-dom'
+import {useForm} from "react-hook-form"
 import { useContext} from "react"
+import {yupResolver} from "@hookform/resolvers/yup"
+import * as yup from "yup"
+import  axios from "axios";
+
 import { Theme } from '../../components/Theme/Theme';
 import { ThemeContext } from "../../context/ThemeProvider" 
 
@@ -20,12 +25,52 @@ const getStyles = (mode) =>({
 
   }
 });
+
+
+const schema = yup
+  .object({
+    Email: yup.string().email().required("Please input your Email"),
+  })
+  .required()
+
+
 const ForgetPassword = () => {
   const {mode} = useContext(ThemeContext);
   const styles = getStyles(mode);
+  const [Loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+
+  const baseURL = "https://waste-project.onrender.com";
+
+
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(schema),
+  })
+  const onSubmit = async(data) => {
+    setLoading(true)
+    const res= await axios.post(
+      `${baseURL}/api/v1/user/forgot-password`,
+      data
+    )
+    console.log(res)
+  setLoading(false)
+  setTimeout(() => {
+    navigate("/ResetPassword")
+  }, 3000);
+
+  };
+
+
+
   return (
     <div className='ForgetContainer'>
-        <div className="SigUpForm" style={styles.background}>
+      
+        <form onSubmit={handleSubmit (onSubmit)} className="SigUpForm" style={styles.background}>
             <h2  style={styles.text}>Oops Forget password?</h2>
             <div className="illus" >
                <img src={FP} alt="FP" />
@@ -34,18 +79,24 @@ const ForgetPassword = () => {
           <input 
             placeholder='Email'
             type="email" 
-            required 
+            {...register("Email")}
+           
             style={styles.background}
+
           />
+           <p className="error">{errors.Email?.message}</p>
+
         </label>
             
-            <Link to="/ResetPassword" style={{textDecoration:"none"}}>
-             <div className='forgetbutton'>   <button >Continue</button></div>
-            </Link>
-            
-        </div>
+             <div className='forgetbutton'>  
+                {Loading? (
+            <p>Loading...</p>
+           ): (<button type="submit">Continue</button>)  }
 
-      
+               </div>
+            
+        </form>
+
     </div>
   )
 }

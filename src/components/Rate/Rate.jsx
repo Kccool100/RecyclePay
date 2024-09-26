32013c8cbe16447c2e8ffb7449efc30625e16e60
Kelechi Anyim <kccool2011@yahoo.com>
2024-09-26 01:33:55 +0100

    rate

--- src/components/Rate/Rate.jsx
+++ src/components/Rate/Rate.jsx
@@ -35,9 +35,9 @@ const Rate = () => {
         </div>
         <div className="rateText">
             <p style={styles.text}>
-            At RecyclePay, we believe in making recycling both simple and rewarding. That’s why we've set a flat rate of ₦400 per kilogram for all types of recyclable material whether you're bringing in plastic, paper, or metal, every kilogram is worth the same. This means that with just 10 kilograms of recyclables, you can earn ₦4,000, and with 20 kilograms, you'll receive ₦8,000. It’s a straightforward way to turn your waste into cash while contributing to a cleaner, greener environment.
+            At RecyclePay, we believe in making recycling both simple and rewarding. That’s why we've set a flat rate of ₦200 per kilogram for all types of recyclable material whether you're bringing in plastic, paper, or metal, every kilogram is worth the same. This means that with just 10 kilograms of recyclables, you can earn ₦2,000, and with 20 kilograms, you'll receive ₦4,000. It’s a straightforward way to turn your waste into cash while contributing to a cleaner, greener environment.
             </p>
-            <div className="kgg" style={styles.text}>1kg = 400 i.e  10kg =4000, 30kg = 12,000</div>
+            <div className="kgg" style={styles.text}>1kg = 200 i.e  10kg =2000, 30kg = 6,000</div>
         </div>
       
     </div>
